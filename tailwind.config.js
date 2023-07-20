5466265cd7d91f14c0e2bcfb201e6287c8abd67d
Alejandra <ale.szuster@gmail.com>
2023-07-20 12:23:16 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      xxl: "1536px",
+      xxxl: "1727px",
+    },
+    colors: {
+      slBeige: "#FDF8ED",
+      slLightBlue: "#7DA2AB",
+      slBlack: "#1D1E16",
+      slGold: "#ECB947",
+      slWine: "#862729",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
